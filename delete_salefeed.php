dd927d9d2e802cee7ea2f84f549747c39d86e554
HashtagF <flamezaxaou1@gmail.com>
2017-07-25 23:27:10 +0700

    dasdas
    asdasdas

--- delete_salefeed.php
+++ delete_salefeed.php
@@ -13,6 +13,15 @@ $username=$_SESSION["Username"];
 
 <?php
     $salefeed_id = $_GET['salefeed_id'];
+    $sel = "SELECT * FROM detail_sale_feed WHERE salefeed_id = '$salefeed_id'";
+    $res = mysql_query($sel,$connect1);
+    while ($row = mysql_fetch_array($res)) {
+      $feed_id = $row['feed_id'];
+      $count = $row['count'];
+      $unit_id = $row['unit_id'];
+      $sql = "INSERT INTO stock_detail (stock_id,mat_id,count,unit_id) VALUES ('MT-06','$feed_id','$count','$unit_id')";
+      mysql_query($sql,$connect1);
+    }
     $sql = "DELETE FROM detail_sale_feed WHERE salefeed_id = '$salefeed_id'";
     mysql_query($sql,$connect1);
     $sql = "DELETE FROM sale_feed WHERE salefeed_id = '$salefeed_id'";
